ec28ac19d0e288ce8b92512ba010a9ca509b9d6c
Cristian <cristian123105@gmail.com>
2019-11-17 15:23:38 -0300

    personalizando btn e arrumando form

--- TODO.md
+++ TODO.md
@@ -2,12 +2,11 @@
     [x] - HTML
     [x] - CSS
     [x] - Fonte
-    [ ] - Estilizar botão
+    [x] - Estilizar botão
     [ ] - Checkbox personalizada
-    [ ] - Dimensões do Form e de alguns inputs
-    [ ] - Setar select default
+    [x] - Dimensões do Form e de alguns inputs
+    [x] - Setar select default
     [ ] - Arrumar labels do checkbox
-    [ ] - Unitilizar select relacionado ao checkbox em "Check the box below"
 [ ] - Validar form com JS
 [ ] - Fazer rotas da aplicação com NodeJS e express
 [ ] - Revisar todo o código
--- frontend/contele/src/pages/Form.js
+++ frontend/contele/src/pages/Form.js
@@ -1,7 +1,8 @@
 import React,{ useState } from 'react';
 import './Form.css'
 function Form() {
-
+    
+    
 
     // Contact Information
     const [name,setName] = useState('');
@@ -26,12 +27,14 @@ function Form() {
     //Checkboxes
     const [vehicleWithFuel,setVehicleWithFuel] = useState(undefined);
     const [tracksOnBike,setTracksOnBike] = useState(undefined);
-    const [identifyDrivers,setidentifyDrivers] = useState(undefined);
-    const [driversIdentify,setDriversIdentify] = useState();
+    const [identifyDrivers,setIdentifyDrivers] = useState(undefined);
+    const [trackersQuantities,setTrackersQuantities] = useState();
+
+
 
     async function handleSubmit(event){
         event.preventDefault();
-        console.log(sameAddres?'mesmo endereço':'outroend');
+        console.log(trackersQuantities);
     }
 
     
@@ -42,7 +45,7 @@ function Form() {
             <div className="section">
                 <div  id="Contact">
                     <h1>Contact Information:</h1>
-                    <input type="text" className="inpuText" value={name} onChange={event => setName(event.target.value)} placeholder="First Name:"/>
+                    <input type="text" className="inpuText"  value={name} onChange={event => setName(event.target.value)} placeholder="First Name:"/>
                     <input type="text" className="inpuText" value={lastname} onChange={event => setLastname(event.target.value)} placeholder="Last Name:"/>
                     <input type="email" className="inpuText" value={email} onChange={event => setEmail(event.target.value)} placeholder="Email Adress:"/>
                     <input type="tel" className="inpuText" value={phone} onChange={event => setPhone(event.target.value)} placeholder="Phone:"/>
@@ -55,7 +58,7 @@ function Form() {
                             <option value="UnitedStates">United States</option>
                             <option value="Mexico">Mexico</option>
                         </select>
-                        <select name="Language"  value={language} onChange={event => setLanguage(event.target.value)} placeholder="Language">
+                        <select name="Language" value={language} onChange={event => setLanguage(event.target.value)} placeholder="Language">
                             <option selected="selected">Language:</option>
                             <option value="Português">Português</option>
                             <option value="English">English</option>
@@ -66,7 +69,7 @@ function Form() {
             </div>
 
             <div className="section">
-                <div action="" id="Shipping" className="">
+                <div action="" id="Shipping" >
                     <h1>Shipping Address:</h1>
                     <input type="text" name="AdressLine1" value={addresShipping1} onChange={event => setAddresShipping1(event.target.value)} className="inpuText" placeholder="Addres Line1:"/>
                     <input type="text" name="AdressLine2" value={addresShipping2} onChange={event => setAddresShipping2(event.target.value)} className="inpuText" placeholder="Addres Line2:"/>
@@ -84,7 +87,7 @@ function Form() {
                             <option value="RJ">Rio de Janeiro</option>
                             <option value="ES">Espirito Santos</option>
                         </select>
-                        <select name="ZIP Code" value={zi} placeholder="ZIP Code">
+                        <select name="ZIP Code" value={shippingzipCode} onChange={event => setShippingZipCode(event.target.value)} placeholder="ZIP Code">
                             <option selected="selected">ZIP Code:</option>
                             <option value="43223">43223</option>
                             <option value="523987">523987</option>
@@ -95,45 +98,52 @@ function Form() {
             </div>
 
             <div className="section"> 
-                <div action="" id="Billing"className="Form">
+                <div action="" id="Billing">
                     <h1>Billing Address:</h1>
-                    <input type="text" className="inpuText"  placeholder="Addres Line1:"/>
-                    <input type="text" className="inpuText"  placeholder="Addres Line2:"/>
+                    <input type="text" className="inpuText" value={addresBilling1} onChange={event => setAddresBilling1(event.target.value)} placeholder="Addres Line1:"/>
+                    <input type="text" className="inpuText" value={addresBilling2} onChange={event => setAddresBilling2(event.target.value)} placeholder="Addres Line2:"/>
                     <div className="selecteds">
-                        <select name="City" id="" placeholder="City">
+                        <select name="City" value={billingCity} onChange={event => setBillingCity(event.target.value)} placeholder="City">
+                            <option selected="selected">City:</option>
                             <option value="Guaruja">Guarujá</option>
                             <option value="Santos">Santos</option>
                             <option value="SaoVicente">São Vicente</option>
                         </select>
-                        <select name="State" id="" placeholder="State">
+                        <select name="State" value={billingState} onChange={event => setBillingState(event.target.value)} placeholder="State">
+                            <option selected="selected">State:</option>
                             <option value="SP">São Paulo</option>
                             <option value="RJ">Rio de Janeiro</option>
                             <option value="ES">Espirito Santos</option>
                         </select>
-                        <select name="ZIP Code" id="" placeholder="ZIP Code">
+                        <select name="ZIP Code" value={billingzipCode} onChange={event => setBillingZipCode(event.target.value)} placeholder="ZIP Code">
+                            <option selected="selected">ZIP Code:</option>
                             <option value="43223">43223</option>
                             <option value="523987">523987</option>
                             <option value="342342">342342</option>
                         </select>
                     </div>
-                    <input type="checkbox" id="ShippingSameBilling" sameAddres={sameAddres} onChange={event => setSameAddres(event.target.value)}/>
+                    <input type="checkbox" id="ShippingSameBilling" className="box" sameAddres={sameAddres} onChange={event => setSameAddres(event.target.value)}/>
                     <label htmlFor="ShippingSameBilling">Use shipping addres same as billing address.</label>
                 </div>
             </div>
 
             <div className="section">
-                <div action="" id="CheckBox"className="Form">
+                <div id="CheckBox">
                     <h1>Check the box below:</h1>
-                    <input type="checkbox" name="vehicleFuel" className="box"/>Does any vehicle need to be equiped with a fuel cut off device?<br/>
-                    <input type="checkbox" name="trackersOnBike" className="box"/>Will any trackers be installed on a bike, truck or machinery?<br/>
-                    <input type="checkbox" name="identifyDrivers" className="box"/>Will you need to identify the fleet drivers?<br/>
-                    <select name="" id="" placeholder="How many trackers would you like to purchase?">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
+                    <input type="checkbox" name="vehicleFuel" vehicleWithFuel={vehicleWithFuel} onChange={event => setVehicleWithFuel(event.target.value)} className="box"/>Does any vehicle need to be equiped with a fuel cut off device?<br/>
+                    <input type="checkbox" name="trackersOnBike"  tracksOnBike={tracksOnBike} onChange={event => setTracksOnBike(event.target.value)} className="box"/>Will any trackers be installed on a bike, truck or machinery?<br/>
+                    <input type="checkbox" name="identifyDrivers" identifyDrivers={identifyDrivers} onChange={event => setIdentifyDrivers(event.target.value)} className="box"/>Will you need to identify the fleet drivers?<br/>
+                    <div className="selecteds">
+                        <select  name="Trackers" value={trackersQuantities}  isDisabled={trackersQuantities} onChange={event => setTrackersQuantities(event.target.value)} placeholder="How many trackers would you like to purchase?">
+                            <option selected="selected">How many trackers would you like to purchase?</option>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
                 </div>
             </div>
             <button className="btn" type="submit">Order Now</button>
