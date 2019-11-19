7ba89cb156458b694d8ce77bfeabe7ee4245c483
Cristian <cristian123105@gmail.com>
2019-11-19 14:01:14 -0300

    add verficaçoes

--- backend/src/controllers/FormController.js
+++ backend/src/controllers/FormController.js
@@ -2,10 +2,8 @@ const Form = require('../models/Form');
 
 module.exports={
     async StorageEvent(request,response){
-    
-        const contact ={
 
-
+        const OrderInfo ={
             name: request.body.name,
             lastname:request.body.lastname,
             email: request.body.email,
@@ -15,49 +13,48 @@ module.exports={
 
             addresShipping1: request.body.addresShipping1,
             addresShipping2: request.body.addresShipping2,
-            shippingCity: request.body.shippingCity,
-            shippingState: request.body.shippingState,
-            shippingzipCode: request.body.shippingzipCode,
+            shippingCity: request.body.shippingCity === 'City:'? '' : request.body.shippingCity,
+            shippingState: request.body.shippingState === 'State:'? '' : request.body.shippingState,
+            shippingzipCode: request.body.shippingzipCode === 'ZIP Code:' ? '' : request.body.shippingzipCode,
 
             addresBilling1: request.body.addresBilling1,
             addresBilling2: request.body.addresBilling2,
-            billingCity: request.body.billingCity,
-            billingState: request.body.billingState,
-            billingzipCode: request.body.billingzipCode,
+            billingCity: request.body.billingCity === 'City:' ? '' : request.body.billingCity,
+            billingState: request.body.billingState === 'State:' ? '' :request.body.billingState,
+            billingzipCode: request.body.billingzipCode === 'ZIP Code:' ? '' :request.body.billingState,
             sameAddres: request.body.sameAddres,
 
+
+        }
+
+        const BoxInfo = {
             vehicleWithFuel: request.body.vehicleWithFuel,
             tracksOnBike: request.body.tracksOnBike,
             identifyDrivers: request.body.identifyDrivers,
             trackersQuantities: request.body.trackersQuantities
         }
+        let ArrayInfo = Object.values(OrderInfo)
+        let Fields = Object.entries(OrderInfo)
+        console.log(Fields+'teste')
+        console.log(ArrayInfo);
+        let missing = [];
+        let valid = [];
+        ArrayInfo.forEach((element,index) => {
+            if(element === undefined | element === '' | element === 0){
+                missing.push(Fields[index])
+            }else{
+                valid.push(element)
+            }
+        });
 
-    const { name } = request.body;
-    const {lastname} = request.body
-    const {email} = request.body
-    const {phone} = request.body
-    /*const {country} = request.bod
-    const {language} = request.body
-
-    const {addresShipping1} = request.body
-    const {addresShipping2} = request.body
-    const {shippingCity} = request.body
-    const {shippingState} = request.body
-    const {shippingzipCode} = request.body
-
-    const {addresBilling1} = request.bod
-    const {addresBilling2} = request.body
-    const {billingCity} = request.body
-    const {billingState} = request.body
-    const {billingzipCode} = request.body
-    const {sameAddres} = request.body
-
-    const {vehicleWithFuel} = request.body
-    const {tracksOnBike} = request.body
-    const {identifyDrivers} = request.body
-    const {trackersQuantities} = request.body*/
-    console.log(contact);
-    return response.json(name);
+        if(missing.length > 0){
+            OrderInfo.complete = false;
+        }else{
+            OrderInfo.complete = true;
+        }
+        OrderInfo.BoxInfo = BoxInfo;
+        console.log(OrderInfo)
+    return response.json(OrderInfo);
 
     }
 }
